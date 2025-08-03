91268c26f9844bc40ed11830b4b8d573e5a6cba2
Saeed-gondle <143780258+Saeed-gondle@users.noreply.github.com>
2025-08-03 15:15:00 +0500

    Learning day # 12

--- Programming With Javascript/Programming Paradigms/Dom-Bom.js
+++ Programming With Javascript/Programming Paradigms/Dom-Bom.js
@@ -4,3 +4,17 @@
 // It allows JavaScript to interact with the browser
 document.body.style.backgroundColor = "lightblue"; // Change background color
 document.body.classList.add("animate-fade-in"); // Add a class for animation
+// Js Selector and Manipulation
+let container = document.querySelector(".container");
+container.style.padding = "20px"; // Add padding to the container
+const heading = document.querySelector(".heading");
+heading.textContent = "Welcome to My Website"; // Change heading text
+heading.style.color = "darkblue"; // Change heading color
+heading.style.padding = "30px"; // Add padding to the heading
+// container.appendChild(heading); // Append heading to the container
+let paragraph = document.querySelector(".paragraph");
+paragraph.textContent = "This is a simple paragraph added using JavaScript."; // Add text to
+let button = document.querySelector("button");
+button.addEventListener("click", function () {
+  alert("Button was clicked!"); // Alert when button is clicked
+});
